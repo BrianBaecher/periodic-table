e4d26296f9b4ff75cab4aa208a3d67d73e5d01ef
Brian Baecher <brian.baecher@gmail.com>
2022-08-07 14:00:31 -0400

    almost done

--- element.sh
+++ element.sh
@@ -4,29 +4,42 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
 ## arguments use ATOMIC NUMBER, SYMBOL, OR NAME
 
-# get atomic number by name
-ATOM_NUM_BY_NAME=$($PSQL "SELECT atomic_number FROM elements WHERE name LIKE '$1'")
-# get atomic number by symbol
-
-# get info by ATOM #
-INFO_BY_NUM=$($PSQL "SELECT atomic_number, name, type, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE atomic_number=$1")
-
-# get symbol
-
-# get type
-
-# get mass
-
-# get melting point
-
-# get boiling point
-if [[ $1 ]]; then
-    if [[ $1 =~ ^[0-9]+$ ]]; then
-        echo $INFO_BY_NUM | while IFS=\| read ATOMIC_NUMBER NAME TYPE SYMBOL MASS MP BP
-        do
-        echo -e "\n The element with atomic number $ATOMIC_NUMBER is $NAME ("$SYMBOL"). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP."| tr -s " " | sed 's/( /(/g' | sed 's/ )/)/g'
-        done
-    fi
+#making variables to check string length later on -- to know what to search by
+STRING=$1
+LENGTH=$(echo $STRING | wc -c)
+#IF ARG IS A NUMBER --- SEARCH BY ATOMIC NUMBER
+if [[ $1 =~ ^[0-9]+$ ]]; then
+    # get info by ATOM #
+    INFO_BY_NUM=$($PSQL "SELECT atomic_number, name, type, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE atomic_number=$1")
+    echo $INFO_BY_NUM | while IFS=\| read ATOMIC_NUMBER NAME TYPE SYMBOL MASS MP BP; do
+        if [[ -z $ATOMIC_NUMBER ]]; then
+            echo -e "I could not find that element in the database."
+        else
+            echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ("$SYMBOL"). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius." | tr -s " " | sed 's/( /(/g' | sed 's/ )/)/g' | sed 's/ ,/,/g'
+        fi
+    done
+#IF ARG IS A STRING OF 1 OR 2 CHARS -- SEARCH BY SYMBOL
+elif [[ $LENGTH -le 3 && $LENGTH -ne 1 ]]; then
+    # get atomic number by symbol
+    INFO_BY_SYMBOL=$($PSQL "SELECT atomic_number, name, type, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE symbol ILIKE '$1'")
+    echo $INFO_BY_SYMBOL | while IFS=\| read ATOMIC_NUMBER NAME TYPE SYMBOL MASS MP BP; do
+        if [[ -z $ATOMIC_NUMBER ]]; then
+            echo -e "I could not find that element in the database."
+        else
+            echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ("$SYMBOL"). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius." | tr -s " " | sed 's/( /(/g' | sed 's/ )/)/g' | sed 's/ ,/,/g'
+        fi
+    done
+#IF ARG IS LONGER THAN 2 CHARS -- SEARCH BY NAME
+elif [[ $LENGTH -gt 3 ]]; then
+    # get atomic number by name
+    INFO_BY_NAME=$($PSQL "SELECT atomic_number, name, type, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE name ILIKE '$1'")
+    echo $INFO_BY_NAME | while IFS=\| read ATOMIC_NUMBER NAME TYPE SYMBOL MASS MP BP; do
+        if [[ -z $ATOMIC_NUMBER ]]; then
+            echo -e "I could not find that element in the database."
+        else
+            echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ("$SYMBOL"). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius." | tr -s " " | sed 's/( /(/g' | sed 's/ )/)/g' | sed 's/ ,/,/g'
+        fi
+    done
 else
     echo Please provide an element as an argument.
 fi
